30e0ccc3efedbc069962cb8be5d1203696f76392
codesigned4 <64992311+codesigned4@users.noreply.github.com>
2021-09-01 04:46:44 +0300

    Update sea_level_predictor.py

--- Sea-level-predictor/sea_level_predictor.py
+++ Sea-level-predictor/sea_level_predictor.py
@@ -3,41 +3,31 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 import numpy as np
 
+
 def draw_plot():
     # Read data from file
-   df=pd.read_csv("epa-sea-level.csv")
-   yearData=np.array(list(df["Year"]))
-   AdjustedSeaLevel=np.array(list(df["CSIRO Adjusted Sea Level"]))
-   
-   # Create scatter plot
-   plt.scatter(yearData,AdjustedSeaLevel,alpha=0.5,label="original data")   
-    # Create first line of best fit
-   slope, intercept, r, p, se = linregress(yearData, AdjustedSeaLevel)
-   #plt.plot(yearData, AdjustedSeaLevel, 'o', label='original data')
-  
-   plt.plot(np.linspace(1880,2050,170), intercept + slope*np.linspace(1880,2050,170), 'r', label='fitted line')
-   
-    # Create second line of best fit
-   df_2000 = df[df['Year'] >= 2000]
+  df=pd.read_csv("epa-sea-level.csv")
+  df.plot.scatter(x='Year', y="CSIRO Adjusted Sea Level",alpha=0.5,label="original data")
+  years = np.arange(1880, 2051)
 
-   np.linspace(2000,2050,50)
-   lineB = linregress(df_2000['Year'], df_2000['CSIRO Adjusted Sea Level'])
-   xB = np.arange(2000,2050,1)
-   yB = xB*lineB.slope + lineB.intercept
+    # First best line
+  slope, intercept, r, p, e  = linregress(df['Year'], df["CSIRO Adjusted Sea Level"])
+  plt.plot(years, intercept + slope*years, 'r', label='best fit line')
 
-   plt.plot(xB,yB) 
-   
-  
-   plt.legend()
+    # Second best line after year 2000
+  recent = df[df['Year'] >= 2000]
+  slope, intercept, r, p, e  = linregress(recent['Year'], recent["CSIRO Adjusted Sea Level"])
 
-    # Add labels and title
-   plt.title('Rise in Sea Level')
-   plt.xlabel('Year')
-   plt.ylabel('Sea Level (inches)')
+  years2 = np.arange(2000, 2051)
+  plt.plot(years2, intercept + slope*years2, 'g', label='second best fit line')
 
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
-   plt.savefig('sea_level_plot.png')
-   return plt.gca()
+  plt.title("Rise in Sea Level")
+  plt.xlabel("Year")
+  plt.ylabel("Sea Level (inches)")
+  plt.legend()
 
+  plt.savefig('sea_level_plot.png')
+  return plt.gca()
+  #plt.show()
 
+#draw_plot()   
